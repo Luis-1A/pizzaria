a13521a18ee91d55d091749b132a9b152c5be41e
Luis Fernando santos <142061648+feito-pelo@users.noreply.github.com>
2024-05-14 13:47:33 -0300

    Update geral.js

--- js/geral.js
+++ js/geral.js
@@ -8,7 +8,7 @@ localStorage.getItem("pizza_cart")
   ? (cart = JSON.parse(localStorage.getItem("pizza_cart")))
   : (cart = []);
 
-const api = fetch("https://raw.githubusercontent.com/feito-pelo/Teste-/main/apiData.json")
+const api = fetch("https://raw.githubusercontent.com/feito-pelo/pizzaria/main/apiData.json")
   .then(async (response) => await response.json())
   .then((data) => {
     pizzas = data;
